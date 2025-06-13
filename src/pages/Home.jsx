e7d1efa8bb9838499cc4cc1da16c1d2352283f39
joejoh <georgeja.18@student.funaab.edu.ng>
2025-06-13 19:05:37 +0100

    modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Home() {
     const [user, setUser] = useState([]); // user is an array
@@ -9,30 +10,24 @@ function Home() {
             const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
             console.log(data); // Log the data to see the structure
             setUser(data);
-        } catch (error) {
+        } catch (error) { 
             console.error("Error fetching data:", error);
         }
     }
- 
+
     useEffect(() => {
         fetchData();
     }, []);
 
     return (
         <div>
-            <div style={{border: "1px solid black", padding: "10px", margin: "10px", width: "300px"}}>
-                {user.length > 0 ? (
-                     user.map((u) => (
-                        <div key={u.id} style={{borderBottom: "1px solid #ccc", marginBottom: "8px", paddingBottom: "8px"}}>
-                            <div>{u.name}</div>
-                            <div>{u.username}</div>
-                            <div>{u.id}</div>
-                        </div>
-                    ))
-                ) : (
-                    <div>Loading...</div>
-                )}
-            </div>
+            {user.map((u) => (
+                <div key={u.id} style={{border: "1px solid #ccc", margin: "10px", padding: "10px"}}>
+                    <div>Name: {u.name}</div>
+                    <div>Username: {u.username}</div>
+                    <div>ID: {u.id}</div>
+                </div>
+            ))}
         </div>
     );
 }
